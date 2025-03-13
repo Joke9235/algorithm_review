841d968491ed3e8ff2cc6a683a5f4df2366eb48d
Kendric <71630561+Joke9235@users.noreply.github.com>
2025-03-13 22:42:45 +0800

    update .gitignore file

--- src/main/java/com/zqr/_day01/_07继承的使用案例/Student.java
+++
@@ -1,8 +0,0 @@
-package com.zqr._day01._07继承的使用案例;
-
-public class Student extends People {
-    // 特有功能
-    public void study(){
-        System.out.println(getName()+"学生认真学习~~~");
-    }
-}
--- src/main/java/com/zqr/_day01/_07继承的使用案例/Teacher.java
+++
@@ -1,8 +0,0 @@
-package com.zqr._day01._07继承的使用案例;
-
-public class Teacher extends People {
-    // 特有功能
-    public void teach(){
-        System.out.println(getName()+"老师要授课");
-    }
-}
